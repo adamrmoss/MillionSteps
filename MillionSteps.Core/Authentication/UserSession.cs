1b3607e24445f05a2197794a17d03d839c71aedf
Adam R Moss <adamrmoss@gmail.com>
2015-10-18 20:10:49 -0400

    WIP

---
+++ MillionSteps.Core/Authentication/UserSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MillionSteps.Core.Authentication
+{
+  public class UserSession
+  {
+    public const string CookieName = "UserSessionId";
+    public static readonly TimeSpan Lifetime = TimeSpan.FromDays(30);
+
+    public Guid Id { get; set; }
+    public string TempToken { get; set; }
+    public string TempSecret { get; set; }
+    public string Verifier { get; set; }
+    public string Token { get; set; }
+    public string Secret { get; set; }
+    public string UserId { get; set; }
+    public DateTime CreateDate { get; set; }
+    public int OffsetFromUtcMillis { get; set; }
+  }
+}
